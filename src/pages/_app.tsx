aa2dd90b02007f351cd6c45ce76c256ba300e7e0
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-19 13:54:14 +0900

    ➕ set up react-modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Modal from 'react-modal'
+
+Modal.setAppElement('#__next')
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
