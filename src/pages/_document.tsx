a91a7fc6426867c02a4e6c5a6976c18f3265b3c9
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-23 22:17:54 +0900

    💄 update font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin />
+            <link href="https://fonts.googleapis.com/css2?family=Actor&display=swap" rel="stylesheet" />
+          </Head>
+          <body className="antialiased">
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+        );
 }
